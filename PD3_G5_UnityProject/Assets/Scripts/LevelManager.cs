a2b17b3882be3ebdadfe6f06a4ab02edc680d381
MarcCornejo <82479432+MarcCornejo@users.noreply.github.com>
2023-04-22 13:10:02 +0200

    SceneController

---
+++ PD3_G5_UnityProject/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] int PATH_LENGHT = 1;
+    static Queue <int> levelPath ;
+    static List <int> allLevels = new List<int> {1,2,1,2};
+    static List<int> levelIndex;
+    //[SerializeField] UnityEvent<float, float> callScene;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelIndex = new List<int>(allLevels);
+        Shuffle(levelIndex);
+        
+
+        for (int i=0;i< allLevels.Count-PATH_LENGHT;i++){
+            levelIndex.RemoveAt(0);   
+        }
+
+        levelPath = new Queue<int>(levelIndex);
+        levelPath.Dequeue();
+        levelPath.Enqueue(1);
+    }
+
+
+    public void LoadLevel(){
+        SceneManager.LoadScene(levelPath.Dequeue());
+    }
+
+
+    public void Shuffle(List<int> alpha)
+    {
+        for (int i = 0; i < alpha.Count; i++)
+        {
+            int temp = alpha[i];
+            int randomIndex = Random.Range(i, alpha.Count);
+            alpha[i] = alpha[randomIndex];
+            alpha[randomIndex] = temp;
+        }
+        foreach (int k in alpha) { Debug.Log(k.ToString()); }
+    }
+
+}
