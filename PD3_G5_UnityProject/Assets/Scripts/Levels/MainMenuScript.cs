4748a3dc15da82b8537f03510555fc7d3eb87d99
MarcCornejo <mcornejo@edu.tecnocampus.cat>
2023-05-05 21:03:34 +0200

    MainMenu

--- PD3_G5_UnityProject/Assets/Scripts/Levels/MainMenuScript.cs
+++ PD3_G5_UnityProject/Assets/Scripts/Levels/MainMenuScript.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    [SerializeField] GameObject options;
+    [SerializeField] GameObject volumeOptions;
+    [SerializeField] GameObject displayOptions;
+    [SerializeField] GameObject ControlsOptions;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
     public void onStartGame()
     {
         SceneManager.LoadScene(0);
@@ -12,11 +21,44 @@ public class MainMenuScript : MonoBehaviour
 
     public void onSettings()
     {
-
+        if(options.activeSelf == false)
+            options.SetActive(true);
+        else
+            options.SetActive(false);
+        if (volumeOptions.activeSelf == true)
+            volumeOptions.SetActive(false);
+        if (displayOptions.activeSelf == true)
+            displayOptions.SetActive(false);
+        if (ControlsOptions.activeSelf == true)
+            ControlsOptions.SetActive(false);
     }
+
+
+    public void onVolumeSettings()
+    {
+        options.SetActive(false);
+        volumeOptions.SetActive(true);
+    }
+
+    public void onDisplaySettings()
+    {
+        options.SetActive(false);
+        displayOptions.SetActive(true);
+    }
+
+    public void onControllerSettings()
+    {
+        options.SetActive(false);
+        ControlsOptions.SetActive(true);
+    }
+
 
     public void onQuit()
     {
         Application.Quit();
     }
+
+
+
+
 }
