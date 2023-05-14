75bf6a3ba270a4068de91d4c137a28b5acedb780
MarcCornejo <82479432+MarcCornejo@users.noreply.github.com>
2023-05-14 13:38:33 +0200

    Initial Golem IA

--- PD3_G5_UnityProject/Assets/GolemEnemyAIScript.cs
+++ PD3_G5_UnityProject/Assets/GolemEnemyAIScript.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
+using UnityEngine.ProBuilder;
 
-public class RangedEnemyAIScript : MonoBehaviour
+public class GolemEnemyAIScript : MonoBehaviour
 {
+
     NavMeshAgent agent;
 
     [SerializeField] LayerMask obstacleMask;
     [SerializeField] GameObject bulletPrefab;
+
     Vector3 distanceToPlayer;
     [SerializeField] UnityEvent<GameObject> objectIsDead;
+    private bool blocked = false;
 
+    //SHOOTING 
     [Header("Forces")]
     [SerializeField] float shootForce;
     [SerializeField] float upwardForce;
@@ -40,10 +45,7 @@ public class RangedEnemyAIScript : MonoBehaviour
 
     enum State { IDLE, CHASE, ATTACK, HIT, DIE }
     [SerializeField] State currentState;
-    [SerializeField] public float secondsToSetEnemy;
-    private bool enemySetted = false;
-
-
+    [SerializeField] float secondsToSetEnemy;
     private GameObject player;
 
     [Header("IDLE")]
@@ -51,7 +53,8 @@ public class RangedEnemyAIScript : MonoBehaviour
 
 
     [Header("CHASE")]
-    [SerializeField] float CHASE_MAX;
+    [SerializeField] float RANGED_RANGE;
+    [SerializeField] float MELEE_RANGE;
 
 
 
@@ -67,6 +70,9 @@ public class RangedEnemyAIScript : MonoBehaviour
     [SerializeField] float fadeSpeed;
     [SerializeField] MeshRenderer enemyRenderer;
 
+
+    private bool enemySetted = false;
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -76,24 +82,12 @@ public class RangedEnemyAIScript : MonoBehaviour
     private void Start()
     {
         player = GameObject.Find("Player");
-        bulletPool = new Queue<GameObject>();
-        GameObject bullets = new GameObject("Bullets");
-        for (int i = 0; i < magazineSize + 10; i++)
-        {
-            GameObject bullet = Instantiate(bulletPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity);
-            bullet.SetActive(false);
-            bulletPool.Enqueue(bullet);
-            bullet.transform.parent = bullets.transform;
-        }
-
-        bulletsLeft = magazineSize;
-        readyToShoot = true;
-        shooting = false;
     }
 
     void Update()
     {
-        if (enemySetted) {
+        if (enemySetted)
+        {
             distanceToPlayer = player.transform.position - transform.position;
             switch (currentState)
             {
@@ -121,7 +115,7 @@ public class RangedEnemyAIScript : MonoBehaviour
                     break;
             }
         }
-        
+
     }
 
 
@@ -130,7 +124,7 @@ public class RangedEnemyAIScript : MonoBehaviour
     void ChangeFromIdle()
     {
         //seesPlayer() &&
-        if (!PlayerInRange())
+        if (!PlayerInMeleeRange())
         {
             currentState = State.CHASE;
         }
@@ -142,9 +136,13 @@ public class RangedEnemyAIScript : MonoBehaviour
     }
 
 
-    bool PlayerInRange()
+    bool PlayerInMeleeRange()
     {
-        return (distanceToPlayer).magnitude < CHASE_MAX;
+        return (distanceToPlayer).magnitude < MELEE_RANGE;
+    }
+    bool PlayerInRangedRange()
+    {
+        return (distanceToPlayer).magnitude < RANGED_RANGE;
     }
 
     bool seesPlayer()
@@ -165,43 +163,101 @@ public class RangedEnemyAIScript : MonoBehaviour
 
     }
 
-
-
-
     void updateChase()
     {
-        if (agent.isStopped) agent.isStopped = !agent.isStopped;
-        agent.SetDestination(player.transform.position);
+        if (agent.isStopped)
+            agent.isStopped = !agent.isStopped;
+
+        if (!blocked)
+            agent.SetDestination(player.transform.position);
     }
 
     void ChangeFromChase()
     {
-        if (PlayerInRange())
+        if (PlayerInMeleeRange() || PlayerInRangedRange())
         {
             currentState = State.ATTACK;
+            attack();
+            if (!blocked)
+                StartCoroutine(CooldownAttack());
         }
         isHit();
     }
 
     private void attack()
     {
-        if (readyToShoot && shooting && !reloading)
+        if (PlayerInMeleeRange())
         {
-            bulletsShot = 0;
-            if (bulletsLeft > 0)
+            player.GetComponent<PlayerHealthScript>().ModifyHealth(damage);
+        }
+        else if(PlayerInRangedRange())
+        {
+            if (readyToShoot && shooting && !reloading)
             {
-                shootingPoint = player.transform.position;
-                agent.isStopped = true;
-                Shoot();
+                bulletsShot = 0;
+                if (bulletsLeft > 0)
+                {
+                    shootingPoint = player.transform.position;
+                    agent.isStopped = true;
+                    Shoot();
+                }
+                else
+                    Reload();
             }
-            else
-                Reload();
         }
     }
 
+    void updateAttack()
+    {
+
+    }
+
+    IEnumerator CooldownAttack()
+    {
+        agent.isStopped = true;
+        yield return new WaitForSeconds(3.0f);
+        agent.isStopped = false;
+    }
+
+    void ChangeFromAttack()
+    {
+        //seesPlayer() &&
+        if (!agent.isStopped && !PlayerInMeleeRange())
+        {
+            currentState = State.CHASE;
+        }
+        isHit();
+    }
+
+    void updateHit()
+    {
+        lastCheckedHealth = GetComponent<EnemyHealthScript>().GetCurrentHealth();
+        if (!blocked)
+            agent.isStopped = true;
+    }
+
+    void ChangeFromHit()
+    {
+        if (lastCheckedHealth <= 0)
+        {
+            currentState = State.DIE;
+        }
+        else
+        {
+            currentState = lastState;
+        }
+    }
+
+    void updateDie()
+    {
+        //objectIsDead.Invoke(gameObject);
+    }
+
+
+
     private void Shoot()
     {
-        
+
         readyToShoot = false;
         Vector3 directionWithoutSpread = shootingPoint - bulletOrigin.position;
 
@@ -255,51 +311,6 @@ public class RangedEnemyAIScript : MonoBehaviour
         reloading = false;
     }
 
-    void updateAttack()
-    {
-        if (PlayerInRange() && readyToShoot && !reloading)
-        {
-            shooting = true;
-            attack();
-        }
-        else
-            shooting = false;
-    }
-
-    void ChangeFromAttack()
-    {
-        //seesPlayer() &&
-        if (!PlayerInRange())
-        {
-            agent.isStopped = false;
-            currentState = State.CHASE;
-        }
-        isHit();
-    }
-
-    void updateHit()
-    {
-        lastCheckedHealth = GetComponent<EnemyHealthScript>().GetCurrentHealth();
-        agent.isStopped = true;
-    }
-
-    void ChangeFromHit()
-    {
-        if (lastCheckedHealth <= 0)
-        {
-            currentState = State.DIE;
-        }
-        else
-        {
-            currentState = lastState;
-        }
-    }
-
-    void updateDie()
-    {
-        //objectIsDead.Invoke(gameObject);
-    }
-
 
 
     private void isHit()
@@ -314,11 +325,46 @@ public class RangedEnemyAIScript : MonoBehaviour
         }
     }
 
+    public void StopAgent()
+    {
+        blocked = true;
+        //agent.SetDestination(transform.position);
+        agent.isStopped = true;
+
+    }
+
+    public void RestartAgent()
+    {
+        blocked = false;
+        //agent.SetDestination(player.transform.position);
+        agent.isStopped = false;
+
+
+    }
+
+    public void GetStunned(float stunTimer)
+    {
+        StartCoroutine(StunEffect(stunTimer));
+    }
+
+    IEnumerator StunEffect(float timer)
+    {
+
+        StopAgent();
+
+        yield return new WaitForSeconds(timer);
+
+        RestartAgent();
+
+
+    }
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(transform.position, CHASE_MAX);
+        Gizmos.DrawWireSphere(transform.position, RANGED_RANGE);
+        Gizmos.DrawWireSphere(transform.position, MELEE_RANGE);
     }
+
 
 
     private void OnEnable()
