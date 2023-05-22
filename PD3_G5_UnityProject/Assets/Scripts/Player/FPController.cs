4c58ea808f99dc00a3918c91f2ded5de0020ad77
MarcCornejo <mcornejo@edu.tecnocampus.cat>
2023-05-22 17:35:25 +0200

    little menu changes

--- PD3_G5_UnityProject/Assets/Scripts/Player/FPController.cs
+++ PD3_G5_UnityProject/Assets/Scripts/Player/FPController.cs
@@ -104,7 +104,7 @@ public class FPController : MonoBehaviour
         ChangeWeapon();
 
         PlayerStatsScript.playerStatsInstance.ActivateBlessings();
-
+        menuPause = GameObject.Find("CanvasPrefab/PauseMenu");
     }
 
     private void FixedUpdate()
