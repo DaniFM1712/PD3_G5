7a14f0a0af052c29b6d04cbf8b319310cada7d1f
MarcCornejo <82479432+MarcCornejo@users.noreply.github.com>
2023-06-14 04:22:52 +0200

    Pantalla de carga

---
+++ PD3_G5_UnityProject/Assets/LoadingScreenScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScreenScript : MonoBehaviour
+{
+    private GameObject blessings;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (LevelManager.instance.getCurrentSceneIndex() < 4)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            blessings = GameObject.Find("BlessingTrigger");
+            blessings.SetActive(false);
+            StartCoroutine(WaitLevelCharge());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator WaitLevelCharge()
+    {
+        yield return new WaitForSecondsRealtime(5f);
+        blessings.SetActive(true); 
+        Destroy(gameObject);
+    }
+
+}
